8aa52df6fe56c1bee551bd46ce7cfc42d21996bf
Te-Wei Tsai <twtsaia@gmail.com>
2020-03-09 17:04:48 -0700

    Add the header file in include directory.

--- src/main.c
+++ src/main.c
@@ -1,9 +1,11 @@
 #include <config.h>
 #include <stdio.h>
+#include <test.h>
 
 int main (void)
 {
-  puts ("Hello World!");
-  puts ("This is " PACKAGE_STRING ".");
+  puts("Hello World!");
+  puts("This is " PACKAGE_STRING ".");
+  printTest();
   return 0;
 }
---
+++ src/test.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <test.h>
+
+void printTest(void) {
+    puts("Print test.");
+}
